8bc392531708bc0749d9651408f7c55f289dddc0
Erika Tharp <eems0926@gmail.com>
2017-01-11 22:12:20 -0800

    This is my portfolio showcasing various projects.

---
+++ main.js
@@ -0,0 +1,11 @@
+function main() {
+    $('.skillset').fadeIn(1000);
+    $('.projects').hide()
+    $('.projects-button').on('click', function(){
+        $(this).next().slideToggle(400);
+        $(this).toggleClass('active');
+        $(this).text("Projects Viewed");
+    });
+}
+
+$(document).ready(main);
